TODO: fix force computation based on obs position (i.e. discard -inf positions)

src/svea_core/src/svea/controllers/mpc.py
@@ -3,7 +3,10 @@ import casadi
 from svea.models.generic_mpc import GenericModel
 
 class MPC(object):
-    def __init__(self, model: GenericModel, x_lb, x_ub, u_lb, u_ub, Q, R, S, N=7, apply_input_noise=False, apply_state_noise=False, verbose=False):
+    # Repulsive force coefficient
+    K_R = 1
+
+    def __init__(self, model: GenericModel, x_lb, x_ub, u_lb, u_ub, n_obstacles, Q, R, S, N=7, apply_input_noise=False, apply_state_noise=False, verbose=False):
         """
         Init method for MPC class
 
@@ -28,6 +31,9 @@ class MPC(object):
         # Get model's number of states and inputs
         self.n_states = len(self.model.dae.x)
         self.n_inputs = len(self.model.dae.u)
+
+        # Get max number of obstacles
+        self.n_obs = n_obstacles
 
         # Check that there are enough lower and upper bounds for each state/input variable
         assert self.n_states == len(x_lb), f'Number of lower bounds does not correspond to states number, number of states: {self.n_states}, number of lower bounds: {len(x_lb)}'
@@ -63,12 +69,15 @@ class MPC(object):
         self.x = None
         self.u = None
         self.reference_state = None
-        self.obs_forces = None
+        self.obs_position = None
 
         # Cost function related varirables
         self.state_diff = []
         self.angle_diff = []
+        self.F_r = []
         self.cost = 0
+
+        self.exps = []
 
         # Verbose option
         self.verbose = verbose
@@ -123,7 +132,7 @@ class MPC(object):
         # Rows as the number of state variables to be kept into accout, columns how many timesteps
         self.reference_state = self.opti.parameter(self.n_states, self.N + 1)
         self.initial_state = self.opti.parameter(self.n_states, 1)
-        self.obs_forces = self.opti.parameter(1, self.N + 1)
+        self.obs_position = self.opti.parameter(2, self.n_obs)
 
     def _optimizer_cost_function(self):
         """
@@ -146,8 +155,17 @@ class MPC(object):
             #self.angle_diff.append(self.reference_state[-1, k] - self.x[-1, k])
             self.cost += self.angle_diff[k]**2 * self.Q[-1, -1]
 
-            # Add cost related to obstacles
-            self.cost += self.S * self.obs_forces[k]
+            # TODO: always loops for 400
+            x_min = casadi.mmin(self.obs_position[0, :])
+            x_max = casadi.mmax(self.obs_position[0, :])
+            
+            index = casadi.find(self.obs_position[0, :] == x_min)
+            # Compute exponential of the repulsive force
+            exp = casadi.sum1(casadi.sqrt((self.x[0, k] - self.obs_position[0, 0:index]) ** 2 + (self.x[1, k] - self.obs_position[1, 0:index]) ** 2))
+            self.exps.append(exp)
+            # Compute repulsive force
+            self.F_r.append(0.5 * self.K_R * casadi.exp(-exp))
+            self.cost += self.S * self.F_r[k]
 
             if k < self.N:
                 # Weight and add to cost the control effort
@@ -173,7 +191,10 @@ class MPC(object):
         # Set initial state as optimizer constraint
         self.opti.subject_to(self.x[:, 0] == self.initial_state)
 
-    def get_ctrl(self, initial_state, reference_state, obs_forces):
+    def set_n_obs(self, n_obs):
+        self.n_obs = n_obs
+
+    def get_ctrl(self, initial_state, reference_state, obs_pos):
         """
         Function to solve optimizer problem and get control from MPC, given initial state and reference state
 
@@ -187,7 +208,7 @@ class MPC(object):
         # Set optimizer values for both initial state and reference states
         self.opti.set_value(self.initial_state, initial_state)
         self.opti.set_value(self.reference_state, reference_state)
-        self.opti.set_value(self.obs_forces, obs_forces)
+        self.opti.set_value(self.obs_position, obs_pos)
         # Solve optimizer problem if it is feasible
         try: 
             self.opti.solve()
@@ -195,9 +216,10 @@ class MPC(object):
             #print(f'Unfeasible state: {self.opti.debug.value(self.x.T)}')
             #print(f'Unfeasible control sequence: {self.opti.value(self.u.T)}')
             print(f'Cost: {self.opti.debug.value(self.cost)}')
-            for angle, state in zip(self.angle_diff, self.state_diff):
+            for angle, state, r_force in zip(self.angle_diff, self.state_diff, self.F_r):
                 print(f'Angle diff: {self.opti.debug.value(angle)}')
                 print(f'State diff: {self.opti.debug.value(state)}')
+                print(f'Repulsive force: {self.opti.debug.value(r_force)}')
             self.opti.debug.show_infeasibilities()
             #self.opti.debug.x_describe()
             #self.opti.debug.g_describe()
@@ -205,11 +227,17 @@ class MPC(object):
         if self.verbose:
             #print(f'Predicted control sequence: {self.opti.value(self.u[:, :])}')
             print(f'Cost: {self.opti.debug.value(self.cost)}')
-            for angle, state in zip(self.angle_diff, self.state_diff):
+            for angle, state, r_force in zip(self.angle_diff, self.state_diff, self.F_r):
                 print(f'Angle diff: {self.opti.debug.value(angle)}')
                 print(f'State diff: {self.opti.debug.value(state)}')
-        print(f'Cost: {self.opti.debug.value(self.cost)}')
-        print(f'Repulsive forces: {self.opti.debug.value(self.obs_forces)}')
+                print(f'Repulsive force: {self.opti.debug.value(r_force)}')
+        for r_force in self.F_r:
+            print(f'MPC Repulsive force: {self.opti.debug.value(r_force)}')
+        #for e in self.exps:
+        #    print(f'MPC EXPS: {self.opti.debug.value(e)}')
+        print(f'MPC Cost: {self.opti.debug.value(self.cost)}')
+        #print(f'MPC State: {self.opti.debug.value(self.x)}')
+        #print(f'MPC Obstacles positions: {self.opti.debug.value(self.obs_position[:, 0:self.n_obs])}')
         # Get first control generated (not predicted ones)
         u_optimal = np.expand_dims(self.opti.value(self.u[:, 0]), axis=1)
         # Get new predicted position

src/svea_social_navigation/scripts/social_navigation.py
@@ -15,7 +15,7 @@ from svea.interfaces import LocalizationInterface, ActuationInterface
 from svea_mocap.mocap import MotionCaptureInterface
 from svea.data import RVIZPathHandler
 from svea_planners.planner_interface import PlannerInterface
-from svea_social_navigation.apf import ArtificialPotentialField
+from svea_social_navigation.apf import ArtificialPotentialFieldHelper
 
 # ROS imports
 from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped
@@ -95,7 +95,7 @@ class SocialNavigation(object):
     GOAL_THRESH = 0.1
     TARGET_VELOCITY = 0.2
     # TODO: set window length on the basis of travelled distance between each timestep (20 or 25 could be fine)
-    WINDOW_LEN = 20
+    WINDOW_LEN = 10
     N_STATES = 4
     MAX_WAIT = 1.0/10.0 # no slower than 10Hz
     K_R = 1000
@@ -126,24 +126,6 @@ class SocialNavigation(object):
         # Publish initial pose
         publish_initialpose(self.state)
 
-        # Create vehicle model object
-        self.model = BicycleModel(initial_state=self.x0, dt=self.DELTA_TIME)
-        # Create MPC controller object
-        self.controller = MPC(
-            self.model,
-            N=self.WINDOW_LEN,
-            Q=[5, 5, 50, 70],
-            R=[1, 2],
-            S=[100],
-            x_lb=[-100, -100, -0.5, -2*np.pi],
-            x_ub=[100, 100, 0.6, 2*np.inf],
-            u_lb=[-1, -np.deg2rad(40)],
-            u_ub=[1.5, np.deg2rad(40)],
-            verbose=False
-        )
-        # Create APF object
-        self.apf = ArtificialPotentialField(svea_name=self.SVEA_NAME, k_r=self.K_R, k_a=self.K_A, window_len=self.WINDOW_LEN)
-
         # Instatiate RVIZPathHandler object if publishing to RVIZ
         self.data_handler = RVIZPathHandler()
 
@@ -166,10 +148,32 @@ class SocialNavigation(object):
                 self.localizer = MotionCaptureInterface(self.SVEA_NAME).start()
             else:
                 self.localizer = LocalizationInterface().start()
-        
+
         # Start simulator
         if self.IS_SIM:
             self.simulator.toggle_pause_simulation()
+
+        # Create APF object
+        self.apf = ArtificialPotentialFieldHelper(svea_name=self.SVEA_NAME, k_r=self.K_R, k_a=self.K_A, window_len=self.WINDOW_LEN)
+        self.apf.wait_for_local_costmap()
+        # Create vehicle model object
+        self.model = BicycleModel(initial_state=self.x0, dt=self.DELTA_TIME)
+        # Create MPC controller object
+        self.controller = MPC(
+            self.model,
+            N=self.WINDOW_LEN,
+            Q=[5, 5, 50, 7],
+            R=[1, 2],
+            S=[10],
+            x_lb=[-100, -100, -0.5, -2*np.pi],
+            x_ub=[100, 100, 0.6, 2*np.inf],
+            u_lb=[-1, -np.deg2rad(40)],
+            u_ub=[1.5, np.deg2rad(40)],
+            n_obstacles=self.apf.get_map_dimensions()[0] * self.apf.get_map_dimensions()[1],
+            verbose=False
+        )
+        # Create matrix of local obstacles
+        self.local_obstacles = np.full((2, self.apf.get_map_dimensions()[0] * self.apf.get_map_dimensions()[1]), np.array([[-100000.0, -100000.0]]).T)
 
     def wait_for_state_from_localizer(self):
         """Wait for a new state to arrive, or until a maximum time
@@ -256,7 +260,7 @@ class SocialNavigation(object):
         # Spin until alive
         while self.keep_alive():
             self.spin()
-            rospy.sleep(0.1)
+            #rospy.sleep(0.1)
         print('ENDING')
 
     def spin(self):
@@ -273,12 +277,14 @@ class SocialNavigation(object):
             self.x0 = [self.sim_model.state.x, self.sim_model.state.y, self.sim_model.state.v, self.sim_model.state.yaw]
             print(f'State: {self.x0}')
     
-        # Get current repulsive force to which the svea is subject 
-        self.repulsive_forces = self.apf.get_repulsive_forces(self.predicted_state.T)
-        is_inf_force = any(f == -np.inf for f in self.repulsive_forces)
-        if is_inf_force: 
-            self.repulsive_forces[:] = 0
-        print(f'Repulsive force: {self.repulsive_forces}')
+        # Fill obstacle array with own position (so that repulsive force is 0)
+        self.local_obstacles = np.full((2, self.apf.get_map_dimensions()[0] * self.apf.get_map_dimensions()[1]), np.array([[-100000.0, -100000.0]]).T)
+        # Get position of obstacles deteceted in the local costmap 
+        obs = np.array(self.apf.get_obstacles_position()).T
+        # If obstacles have been detected, insert them into the array
+        if len(obs) > 0:
+            self.local_obstacles[:, 0:np.shape(obs)[1]] = obs
+        print(self.local_obstacles[:, 0:np.shape(obs)[1]])
 
         # TODO: use lateral offset from path to get next waypoint
         while np.linalg.norm(self.x0[0:2] - self.path[self.waypoint_idx, 0:2]) < self.GOAL_THRESH and self.waypoint_idx < np.shape(self.path)[0] - 1:
@@ -293,9 +299,9 @@ class SocialNavigation(object):
             last_iteration_points = self.path[self.waypoint_idx:, :]
             while np.shape(last_iteration_points)[0] < self.WINDOW_LEN + 1:
                 last_iteration_points = np.vstack((last_iteration_points, self.path[-1, :]))
-            u, self.predicted_state = self.controller.get_ctrl(self.x0, last_iteration_points[:, :].T, self.repulsive_forces)
+            u, self.predicted_state = self.controller.get_ctrl(self.x0, last_iteration_points[:, :].T, self.local_obstacles)
         else:
-            u, self.predicted_state = self.controller.get_ctrl(self.x0, self.path[self.waypoint_idx:self.waypoint_idx + self.WINDOW_LEN + 1, :].T, self.repulsive_forces)
+            u, self.predicted_state = self.controller.get_ctrl(self.x0, self.path[self.waypoint_idx:self.waypoint_idx + self.WINDOW_LEN + 1, :].T, self.local_obstacles)
 
         # Get optimal velocity and steering controls
         velocity = u[0, 0]

src/svea_social_navigation/src/svea_social_navigation/apf.py
@@ -7,14 +7,13 @@ import rospy
 # ROS imports
 from nav_msgs.msg import OccupancyGrid
 from geometry_msgs.msg import PointStamped
-from svea_msgs.msg import VehicleState as VehicleStateMsg
 
 def load_param(name, value=None):
     if value is None:
         assert rospy.has_param(name), f'Missing parameter "{name}"'
     return rospy.get_param(name, value)
 
-class ArtificialPotentialField(object):
+class ArtificialPotentialFieldHelper(object):
     """
     Class that given a local costmap computes the APF (only related to obstacles) for given robot 
     """
@@ -83,58 +82,39 @@ class ArtificialPotentialField(object):
         self._map_x = msg.info.origin.position.x
         self._map_y = msg.info.origin.position.y
         self._local_costmap = np.reshape(msg.data, (self._map_height, self._map_width))
+        
+    def get_obstacles_position(self):
+        """
+        Function to retrieve obstacles position, given a local costmap
 
-    def _compute_attractive_forces(self):
+        :return: position of obstacles
+        :rtype: list[tuple[float]]
         """
-        Method to compute attractive force
+        if self._local_costmap is not None:
+            # Get obstacles indexes
+            obs_indexes = np.transpose((self._local_costmap > 70).nonzero())
+            obs_positions = np.zeros(np.shape(obs_indexes))
+            obs_positions[:, 1] = obs_indexes[:, 0] * self._map_resolution + self._map_y
+            obs_positions[:, 0] = obs_indexes[:, 1] * self._map_resolution + self._map_x
+            print(f'APF : {obs_positions}')
+            return obs_positions
+        else:
+            return None
+        
+    def get_map_dimensions(self):
         """
-        if self._svea_states is not None and self._waypoint is not None:
-            for i, x in enumerate(self._svea_states):
-                self.F_a[i] = 0.5 * self.k_a * np.sqrt((x[i, 0] - self._waypoint[0]) ** 2 + (x[i, 1] - self._waypoint[1]) ** 2)
-            return self.F_a
+        Function to retrieve the map's dimensions
 
-    def _compute_repulsive_forces(self):
+        :return: maps dimensions
+        :rtype: int, int
         """
-        Method to compute repulsive force
-        """
-        if self._svea_states is not None and self._local_costmap is not None:
-            for i, x in enumerate(self._svea_states):
-                # Get obstacles indexes
-                obs_indexes = np.transpose((self._local_costmap > 70).nonzero())
-                # If obstacles are detected
-                if len(obs_indexes) > 0:
-                    # Compute obstacles positions wrt map frame
-                    obs_positions = np.zeros(np.shape(obs_indexes))
-                    obs_positions[:, 1] = obs_indexes[:, 0] * self._map_resolution + self._map_y
-                    obs_positions[:, 0] = obs_indexes[:, 1] * self._map_resolution + self._map_x 
-                    # TODO: each state needs to be related to the simulated local costmap (wrt to controls)
-                    # Compute exponential of the repulsive force
-                    exp = sum([np.sqrt((x[0] - obs[0]) ** 2 + (x[1] - obs[1]) ** 2) for obs in obs_positions])
-                    # Compute repulsive force
-                    self.F_r[i] = 0.5 * self.k_r * np.exp(-exp)
-            return self.F_r
-
-    def get_repulsive_forces(self, x):
-        """
-        Method to get the array of predicted repulsive forces given the array of predicted state, plus current one
-
-        :param x: current position, plus predicted ones
-        :type x: list[tuple[float]
-        """
-        self._set_states(x)
-        return self._compute_repulsive_forces()
-
-    def get_attractive_forces(self, x):
-        """
-        Method to get the array of predicted attractive forces given the array of predicted state, plus current one
-
-        :param x: current position, plus predicted ones
-        :type x: list[tuple[float]
-        """
-        self._set_states(x)
-        return self._compute_attractive_forces()
-
-    def get_resultant_forces(self):
+        if self._map_height is not None and self._map_width is not None:
+            return self._map_height, self._map_width
+        
+    def wait_for_local_costmap(self):
+        while self._local_costmap is None:
+            rospy.sleep(1)
+        
         """
         Function to retrieve the resultant force
 
@@ -147,8 +127,9 @@ class ArtificialPotentialField(object):
 if __name__ == '__main__':
     ## Start node ##
     rospy.init_node('apf_node')
-    a = ArtificialPotentialField(k_a=10, k_r=100, window_len=20)
+    a = ArtificialPotentialFieldHelper(k_a=10, k_r=100, window_len=20)
     a.get_repulsive_forces([[5.60878716, 6.7788254 ],[5.60731558, 6.80588076],[5.60621237, 6.83228813],[5.60550238, 6.85865974],[5.60524131, 6.88509128],[5.6055639 , 6.91207052],[5.60697562, 6.94083662],[5.61051214, 6.97177156],[5.61671512, 7.00309276],[5.62532682, 7.03331632],[5.6359971 , 7.06185886],[5.64805739, 7.0883965 ],[5.66084955, 7.11314632],[5.67406027, 7.13648677],[5.68749305, 7.15861945],[5.70096879, 7.17964629],[5.71433584, 7.19964465],[5.72746704, 7.21868267],[5.74026105, 7.23683041],[5.75264475, 7.25416544],[5.76450291, 7.27066924]])
     rospy.spin()
 
-        
+
+[2.4        ,2.4,        2.4,        2.60000001, 2.60000001, 2.60000001,  2.80000001, 2.80000001, 5.00000004, 5.00000004, 5.00000004, 5.00000004]        