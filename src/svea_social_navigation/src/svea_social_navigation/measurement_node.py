bdd1ee4deb4f9f27cb3b23ed6390dbce4af3d86e
Fedezac <fedezac99@gmail.com>
2023-05-22 15:52:10 +0200

    Todo fix arc visualization

--- src/svea_social_navigation/src/svea_social_navigation/measurement_node.py
+++ src/svea_social_navigation/src/svea_social_navigation/measurement_node.py
@@ -2,6 +2,8 @@
 # Plain python imports
 import numpy as np
 import re
+from matplotlib import pyplot as plt
+import matplotlib.patches as mpatches
 
 class SocialMeasurement(object):
     SVEA_FILE = '/home/federico/universita/thesis_ws/ws/src/svea_social_navigation/data/svea_states.txt'
@@ -80,10 +82,47 @@ class SocialMeasurement(object):
             old_states.append(list(array[1:]))
             self.pedestrian_states.update({int(array[0]): old_states})
 
+    def get_personal_space(self, pose):
+        # Arc1
+        if pose[2] < 1.06:
+            a = 0.54 * pose[2] + 0.33
+        elif pose[2] < 1.26:
+            a = 3.0
+        else:
+            a = 10.41 * pose[2] - 10.10
+        b = 0.14 * a
+        arc1 = [pose[0], pose[1], np.abs(b * np.cos(pose[3])), np.abs(b * np.sin(pose[3])), np.pi]
+
+        # Arc 2
+        e = np.sqrt(a ** 2 + b ** 2)
+        d = a - b
+        r = (e * (e - d)) / (2 * a)
+        og = a - r
+        g = pose[0:2] + og
+        alpha = np.arctan2(b, a) 
+        arc2 = [g[0], g[1], r * np.cos(pose[3]), r * np.sin(pose[3]), 2 * alpha]
+
+        # Arc 3
+        R = (e * (e + d)) / (2 * b)
+        of = R - b
+        f = pose[0:2] + of
+        beta = np.arctan2(a, b)
+        arc3 = [f[0], f[1], R * np.cos(pose[3]), R * np.sin(pose[3]), 2 * beta]
+
+        return arc1, arc2, arc3
+
+
 
 if __name__ == '__main__':
     m = SocialMeasurement()
     m.read_robot_poses()
     m.read_pedestrian_poses()
-    print(len(m.svea_states))
-    print(len(m.pedestrian_states.get(0)))
+    for key in m.pedestrian_states:
+        for pose in m.pedestrian_states[key]:
+            arc1, arc2, arc3 = m.get_personal_space(pose)
+            print(arc1)
+            plt.clf()
+            plt_arc1 = mpatches.Arc((arc1[0], arc1[1]), arc1[2], arc1[3], angle=arc1[4], color="orange")
+            plt.gcf().gca().add_artist(plt_arc1)
+            plt.show()  
+            plt.pause(0.01)
